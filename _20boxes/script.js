805898cf6269b0f544f638de662577c0c633963e
bo-99 <76881580+bo-99@users.noreply.github.com>
2021-01-09 23:17:36 +0100

    4.task

---
+++ _20boxes/script.js
@@ -0,0 +1,54 @@
+//console.log("boglarka");
+
+//let myVariable = true;
+//if (myVariable === true) {
+ // console.log('The condition has been evaluated to true');
+//};
+
+
+//let add = (a, b) => {
+//return a * b;  // the return keyword will give you back the result
+ // };
+  
+ // console.log(add(5, 9));
+
+
+//  let bookCount = 10;
+//  for (let i = 0; i < bookCount; i++) {console.log(i)};
+
+
+// function printNumbersTill(a) {
+//  for (let i = 1; i < a+1; i++) {console.log(i)};
+// }
+// printNumbersTill(15)
+
+
+
+//function getGreetingTo(name) {
+//  console.log('Hello ' + name)
+//} 
+// getGreetingTo('Mark')
+
+
+let colors = ['blue', 'white', 'red'];
+function printValues(array) {
+  for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);}
+}
+  printValues(colors)
+
+
+ //let myNumbers = [10, 20, 30, 50, 12];
+ // myNumbers.forEach((number) => {
+//       console.log(number);
+ // });
+
+
+ let colori = ['orange', 'blue', 'white', 'red'];
+ colori.forEach((colore) => {
+    console.log(colore);
+  })
+
+
+
+
