dd4439afddf8f793c009ec1feb4066626db28b05
bo-99 <76881580+bo-99@users.noreply.github.com>
2021-01-10 16:40:01 +0100

    egtajak

---
+++ 5 jquery/script.js
@@ -0,0 +1,16 @@
+let color = "purple";
+let number = 10;
+let word = "cool";
+
+$('.red').css('background', color);
+$('.yellow').text('North-East');
+if (number > 100) {
+    console.log("whoah, that's a big number") } else
+    {console.log("just a regular number, please");
+}
+if (word === 'cool') {
+    $('.turq').text("power of DOM")} else
+    {$('.blue').text("power of DOM");
+}
+
+
